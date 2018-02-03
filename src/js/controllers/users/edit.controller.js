73a100de8ea08908a644fad8454fbcfc6b7fd361
Otis <otisvickersgraver@gmail.com>
2018-02-03 19:21:33 +0000

    added js/users controller functionality,  js/factories/user factory, ng-app directive, app.js directives

---
+++ src/js/controllers/users/edit.controller.js
@@ -0,0 +1,11 @@
+// angular
+//   .module('vamApp')
+//   .controller('UsersEditCtrl', UsersEditCtrl);
+//
+// UsersEditCtrl.$inject = ['$state', 'UserFactory'];
+//
+// function UsersEditCtrl($state, UserFactory) {
+//   const vm = this;
+//
+//
+// }
