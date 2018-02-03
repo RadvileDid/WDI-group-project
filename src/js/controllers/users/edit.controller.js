2b410d69311f18e593cbbd3fed13d17aeaed4f01
Otis <otisvickersgraver@gmail.com>
2018-02-03 21:13:52 +0000

    added ui-view to main in index.html, completed user routes, added user view files

--- src/js/controllers/users/edit.controller.js
+++ src/js/controllers/users/edit.controller.js
@@ -1,11 +1,23 @@
-// angular
-//   .module('vamApp')
-//   .controller('UsersEditCtrl', UsersEditCtrl);
-//
-// UsersEditCtrl.$inject = ['$state', 'UserFactory'];
-//
-// function UsersEditCtrl($state, UserFactory) {
-//   const vm = this;
-//
-//
-// } //WORKING ON THIS
+angular
+  .module('vamApp')
+  .controller('UsersEditCtrl', UsersEditCtrl);
+
+UsersEditCtrl.$inject = ['$state', 'UserFactory'];
+
+function UsersEditCtrl($state, UserFactory) {
+  const vm = this;
+
+  vm.user = UserFactory.get($state.params);
+  vm.userUpdate = userUpdate;
+
+  function userUpdate(){
+    UserFactory
+      .update($state.params, vm.user)
+      .$promise
+      .then(() => {
+        $state.go('SOMEWHERE'); // go to users profile once their profile has been updated. from router config
+      });
+  }
+
+
+}
--- src/js/controllers/users/show.controller.js
+++ src/js/controllers/users/show.controller.js
@@ -2,16 +2,17 @@ angular
   .module('vamApp')
   .controller('UsersShowCtrl', UsersShowCtrl);
 
-UsersShowCtrl.$inject = ['$state', 'User'];
+UsersShowCtrl.$inject = ['$state', 'UserFactory'];
 
-function UsersShowCtrl($state, User) {
+function UsersShowCtrl($state, UserFactory) {
   const vm = this;
 
-  vm.user = User.get($state.params);
+  vm.user = UserFactory.get($state.params);
   vm.userDelete = userDelete;
 
   function userDelete(){
-    User.delete($state.params)
+    UserFactory
+      .delete($state.params)
       .$promise
       .then(() => {
         $state.go('SOMEWHERE'); // where do we want the user to go once their profile has been deleted? from router config
