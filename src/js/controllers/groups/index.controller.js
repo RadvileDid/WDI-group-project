74eea415ed2bf288c834f58ec6c8292fc3fc0791
Hannah Tucker McLellan <hannahjt.703@gmail.com>
2018-02-05 15:22:19 +0000

    movies display, working on the search

--- src/js/controllers/groups/index.controller.js
+++ src/js/controllers/groups/index.controller.js
@@ -2,18 +2,32 @@ angular
   .module('vamApp')
   .controller('GroupsIndexCtrl', GroupsIndexCtrl);
 
-GroupsIndexCtrl.$inject = ['$http'];
-function GroupsIndexCtrl($http) {
+GroupsIndexCtrl.$inject = ['$http', '$scope', 'filterFilter'];
+function GroupsIndexCtrl($http, $scope, filterFilter) {
   const apiKey = '1d4fa77475568ca9a63fb4a287dd496b';
   const query = 'neverending%20story';
+  //  brewdog and coffee
   const vm = this;
 
   $http
     .get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}&include_adult=false`)
     .then(res => {
-      console.log(res.data);
-      vm.all = res.data;
+      console.log(res.data.results);
+      vm.all = res.data.results;
     });
+
+  vm.all = Group.query();
+
+  function filterGroup() {
+    const params = { name: vm.query };
+    vm.filtered = filterFilter(vm.all, params);
+  }
+
+  $scope.$watchGroup([
+    () => vm.query
+  ], filterGroup);
+
+  vm.filterGroup = filterGroup;
 
 }
 
