67e8c7b1cc3135610690dad8a1488797df43ab62
RadvileDid <radvilesubaciute@gmail.com>
2018-02-05 11:39:39 +0000

    created basic navbar and linked these states

--- src/js/configs/router.config.js
+++ src/js/configs/router.config.js
@@ -7,6 +7,10 @@ function Router($stateProvider, $urlRouterProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 
   $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'js/views/static/home.html'
+    })
     .state('login', {
       url: '/login',
       templateUrl: '/js/views/auth/login.html',
