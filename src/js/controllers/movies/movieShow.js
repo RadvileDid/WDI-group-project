f78ad1f0bed8a3e3158b07f23e21a5f3aa0d3247
RadvileDid <radvilesubaciute@gmail.com>
2018-02-05 21:03:29 +0000

    single movie page is half working

---
+++ src/js/controllers/movies/movieShow.js
@@ -0,0 +1,18 @@
+angular
+  .module('vamApp')
+  .controller('MovieCtrl', MovieCtrl);
+
+MovieCtrl.$inject = ['$http', '$state'];
+
+function MovieCtrl($http, $state) {
+  const apiKey = '1d4fa77475568ca9a63fb4a287dd496b';
+  const vm = this;
+
+  $http
+    .get(`https://api.themoviedb.org/3/movie/${$state.params}?api_key=${apiKey}`)
+    .then(res => {
+      // console.log(res.data.results);
+      console.log($state.params.id);
+      vm.all = res.data.results;
+    });
+}
