339754716300d50bb30260126bc485942095f83a
Otis <otisvickersgraver@gmail.com>
2018-02-05 11:13:41 +0000

    added edit username form

--- src/js/factories/user.factory.js
+++ src/js/factories/user.factory.js
@@ -2,10 +2,9 @@ angular
   .module('vamApp')
   .factory('UserFactory', UserFactory);
 
-UserFactory.$inject = ['$resource']; //'API'
+UserFactory.$inject = ['$resource'];
 function UserFactory($resource) {
-  return new $resource('/api/users/:id', //${API}
-    { id: '@_id'},
+  return new $resource('/api/users/:id',
     { 'update': { method: 'PUT' }
     }
   );
