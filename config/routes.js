74d4f5a40557100f8629cdf6466be7568f6c99eb
Otis <otisvickersgraver@gmail.com>
2018-02-05 13:07:23 +0000

    can only see delete and edit profile if user is logged in

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,4 @@
- const express = require('express');
+const express = require('express');
 const router  = express.Router();
 const auth = require('../controllers/auth');
 const users = require('../controllers/users');
