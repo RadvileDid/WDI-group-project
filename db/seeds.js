e3cb18db313cf41d4139fd3662bf0d6e88776fef
Otis <otisvickersgraver@gmail.com>
2018-02-05 09:51:31 +0000

    fixed db & env in seeds

--- db/seeds.js
+++ db/seeds.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const { db } = require('../config/environment'); //dev db needs to be the db "development" from the environment.js file. I am not sure how to direct it there but I will work on that now.
+const { db, env } = require('../config/environment'); //dev db needs to be the db "development" from the environment.js file. I am not sure how to direct it there but I will work on that now.
 
 mongoose.Promise = require('bluebird');
-mongoose.connect(db);
+mongoose.connect(db[env]);
 
 const Group = require('../models/group');
 const User = require('../models/user');
