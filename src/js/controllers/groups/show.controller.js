9a5d68af894f6189899e7f65743500cc2a142db6
Otis <otisvickersgraver@gmail.com>
2018-02-06 11:07:34 +0000

    refactored show controller and added addGroup function

--- src/js/controllers/groups/show.controller.js
+++ src/js/controllers/groups/show.controller.js
@@ -2,25 +2,85 @@ angular
   .module('vamApp')
   .controller('GroupsShowCtrl', GroupsShowCtrl),
 
-GroupsShowCtrl.$inject = ['$state', '$http'];
-function GroupsShowCtrl($state, $http) {
+GroupsShowCtrl.$inject = ['Group', '$state', '$auth'];
+function GroupsShowCtrl(Group, $state, $auth) {
   const vm = this;
+  vm.group = Group.get($state.params.id);
 
-  $http
-    .get(`/api/groups/${$state.params.id}`)
-    .then((response) => {
-      vm.group = response.data;
-    });
+
+  function deleteGroup() {
+    vm.group
+      .$remove()
+      .then(() =>
+        $state.go('groupsIndex'));
+  }
 
   vm.delete = deleteGroup;
 
-  function deleteGroup() {
-
-    $http
-      .delete(`/api/groups/${$state.params.id}`)
-      .then(() => {
-        $state.go('groupsIndex');
-      });
+  function addGroup() {
+    Group
+      .joinGroup({ id: vm.user.id }) //??????????
+      .$promise
+      .then((response) => vm.group.add = response.add);
   }
 
+  vm.add = addGroup;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//hannahs $http
+
+// angular
+//   .module('vamApp')
+//   .controller('GroupsShowCtrl', GroupsShowCtrl),
+//
+// GroupsShowCtrl.$inject = ['$state', '$http'];
+// function GroupsShowCtrl($state, $http) {
+//   const vm = this;
+//
+//   $http
+//     .get(`/api/groups/${$state.params.id}`)
+//     .then((response) => {
+//       vm.group = response.data;
+//     });
+//
+//   vm.delete = deleteGroup;
+//
+//   function deleteGroup() {
+//
+//     $http
+//       .delete(`/api/groups/${$state.params.id}`)
+//       .then(() => {
+//         $state.go('groupsIndex');
+//       });
+//   }
+//
+// }
