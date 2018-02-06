0fed9c2305d46591c81b4232586eb8d72142692b
RadvileDid <radvilesubaciute@gmail.com>
2018-02-06 11:18:14 +0000

    fixed file names

--- src/js/controllers/groups/show.controller.js
+++ src/js/controllers/groups/show.controller.js
@@ -1,26 +1,42 @@
 angular
   .module('vamApp')
-  .controller('GroupsShowCtrl', GroupsShowCtrl),
+  .controller('MovieCtrl', MovieCtrl);
 
-GroupsShowCtrl.$inject = ['$state', '$http'];
-function GroupsShowCtrl($state, $http) {
+MovieCtrl.$inject = ['$http', '$state', 'MovieComment'];
+
+function MovieCtrl($http, $state, MovieComment) {
+  const apiKey = '1d4fa77475568ca9a63fb4a287dd496b';
   const vm = this;
 
   $http
-    .get(`/api/groups/${$state.params.id}`)
-    .then((response) => {
-      vm.group = response.data;
+    .get(`https://api.themoviedb.org/3/movie/${$state.params.id}?api_key=${apiKey}`, { skipAuthorization: true })
+    .then(res => {
+      console.log(res);
+      vm.movie = res.data;
     });
 
-  vm.delete = deleteGroup;
-
-  function deleteGroup() {
-
-    $http
-      .delete(`/api/groups/${$state.params.id}`)
-      .then(() => {
-        $state.go('groupsIndex');
+  function addComment() {
+    MovieComment
+      .save({ id: vm.movie.id }, vm.newComment)
+      .$promise
+      .then((comment) => {
+        vm.movie.comments.push(comment);
+        vm.newComment = {};
       });
   }
+
+  vm.addComment = addComment;
+  //
+  // function deleteComment(comment) {
+  //   MovieComment
+  //     .delete({ movieId: vm.movie.id, id: comment.id })
+  //     .$promise
+  //     .then(() => {
+  //       const index = vm.movie.comments.indexOf(comment);
+  //       vm.movie.comments.splice(index, 1);
+  //     });
+  // }
+  //
+  // vm.deleteComment = deleteComment;
 
 }
