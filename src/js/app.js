6411462ee32a66625ecc457b341443e4cb5e5370
RadvileDid <radvilesubaciute@gmail.com>
2018-02-08 12:11:00 +0000

    fixed movie index show page and the no image error

--- src/js/app.js
+++ src/js/app.js
@@ -3,4 +3,14 @@ angular
     'ui.router',
     'ngResource',
     'satellizer'
-  ]);
+  ])
+  .directive('onError', function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attr) {
+        element.on('error', function() {
+          element.attr('src', attr.onError);
+        });
+      }
+    };
+  });
